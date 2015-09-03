bcfebe10cd4447b07dbda8478731b2fc1c569377
natt <za_sassygirl@hotmail.com>
2015-09-03 12:38:10 +0700

    Start Project

--- app/src/main/java/com/fiftheleven/natt/nattrestaurant/FoodTABLE.java
+++ app/src/main/java/com/fiftheleven/natt/nattrestaurant/FoodTABLE.java
@@ -1,7 +1,32 @@
 package com.fiftheleven.natt.nattrestaurant;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by admin on 9/3/2015.
  */
 public class FoodTABLE {
+
+    //Explicit
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public static final String FOOD_TABLE = "foodTABLE";
+    public static final String COLUMN_ID_FOOD = "_id";
+    public static final String COLUMN_FOOD = "Food";
+    public static final String COLUMN_SOURCE = "Source";
+    public static final String COLUMN_PRICE = "Price";
+
+    public FoodTABLE(Context context) {
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
+    }   //Constructor
+
+    public long addNewFood(String strFood, String strSource, String strPrice) {
+        ContentValues objContentValues = new ContentValues();
+        return 0;
+    }
 }
