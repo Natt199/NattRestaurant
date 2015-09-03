ccf987d0363d95aa8860d1894ca43be18a34c227
natt <za_sassygirl@hotmail.com>
2015-09-03 11:31:29 +0700

    Create SQLite

---
+++ app/src/main/java/com/fiftheleven/natt/nattrestaurant/UserTABLE.java
@@ -0,0 +1,23 @@
+package com.fiftheleven.natt.nattrestaurant;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by admin on 9/3/2015.
+ */
+public class UserTABLE {
+
+    //Explicit
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public UserTABLE(Context context) {
+
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
+
+    }   //Constructor
+}   //Main Class
+
